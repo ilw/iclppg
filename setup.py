0cf338480bc740ddd9f05e082a992c02c59d37d9
ilw <ian_l_williams@hotmail.com>
2020-07-01 17:39:27 +0100

    added mainwindow.ui to the setup.py package data list

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ilw/iclppg",
     packages=setuptools.find_packages(),
+    package_data={
+    'GUI': ['mainWindow.ui'],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT ",
