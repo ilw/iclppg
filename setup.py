1fcc9e63c6806a0d47ebaaadc1ef0e91f8dd23c8
ilw <ian_l_williams@hotmail.com>
2020-07-01 17:24:08 +0100

    Added first attempt at setup.py to support automatic update

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iclppg", # Replace with your own username
+    version="0.1",
+    author="Ian Williams",
+    author_email="",
+    description="Raspberry pi - Max30101 ppg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ilw/iclppg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT ",
+        "Operating System :: Raspberry Pi Debian",
+    ],
+    python_requires='>=3.6',
+)
